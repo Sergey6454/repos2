701d9b2519b9f13580ab68a1c1ed7e0e1cc3340f
Sergei <SergeiEmail>
2022-06-29 19:30:23 +0500

    start

---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            while (true)
+            {
+                Console.WriteLine("1-Ввести А\n" +
+                     "2-Ввести В\n" +
+                     "3-Сложить А и В\n" +
+                     "4-Вычесть В из А\n" +
+                     "5-Умножить А на В\n" +
+                     "6-Делить В на А\n");
+                int rez = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
